bcfaa2bbd49a4372c0c4345d17c5dfd775c38814
Diego Pino Garcia <dpino@igalia.com>
2025-04-02 16:20:48 +0200

    ci: Allow running single test or group of tests
    When launching 'run-tests.sh' allow passing list of tests to run.
    Otherwise, run all Robot tests.
    
    Maintenance-type: ci

--- .ci/robot_framework/run-robot.sh
+++ .ci/robot_framework/run-robot.sh
@@ -30,10 +30,25 @@ then
     ../../prepare-board.sh
 fi
 
+# Set tests to run.
+TESTS_DIR="../../robot_framework/tests"
+TESTS=()
+
+ARGS=($@)
+for each in ${ARGS[@]}; do
+    testname="$TESTS_DIR/$(basename "$each")"
+    TESTS+=($testname)
+done
+
+# In case no test set, run all tests.
+if [[ ${#TESTS[@]} -eq 0 ]]; then
+    TESTS+=("$TESTS_DIR/tests_*.robot")
+fi
+
+# Run tests.
 exec robot --name "WPE image tests" \
            --consolewidth 158 \
            --exclude skip \
            --skiponfailure ignoreonfail \
            --listener RetryFailed:2 \
-           ../../robot_framework/tests/tests_*.robot
-
+           "${TESTS[@]}"
