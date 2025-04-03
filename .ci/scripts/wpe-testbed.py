44595896c4aa1ef0028808decf5def6e1b3e5d63
Diego Pino Garcia <dpino@igalia.com>
2025-04-03 15:05:50 +0200

    ci,tests: Add wpe-testbed tests
    Maintenance-Type: ci

---
+++ .ci/scripts/wpe-testbed.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import os
+import re
+import subprocess
+import sys
+
+import xml.etree.ElementTree as ET
+
+TESTS = ('unbounded', 'unbounded_and_no_animate')
+
+def parse_args(argument_list):
+    parser = argparse.ArgumentParser(
+        description="Run WPE testbed.",
+    )
+    parser.add_argument('--new-baseline', action='store_true', dest='new_baseline', help='Save generated results as new baseline')
+    parser.add_argument('--report', action='store_true', dest='report', help='Return actual and expected results')
+    parser.add_argument('--filter', dest='filter', choices=TESTS, help='Filter by test name')
+    return parser.parse_args(argument_list)
+
+# Utility class to compose run wpe-testbed.
+class WPETestbed:
+
+    program = "wpe-testbed-wayland"
+
+    def __init__(self, **kwargs):
+        # Set default values.
+        if "tiles" not in kwargs:
+            self.tiles = 1
+        if not "opaque" in kwargs:
+            self.opaque = True
+        if not "fences" in kwargs:
+            self.fences = True
+        if not "title_update_method" in kwargs:
+            self.title_update_method = "mmap"
+        if not "dmabuf_tiles" in kwargs:
+            self.dmabug_tiles = True
+        if not "frames" in kwargs:
+            self.frames = 2000
+        if not "drm_node_ipu" in kwargs:
+            self.drm_node_ipu = "/dev/dri/card0"
+        if not "drm_node_gpu" in kwargs:
+            self.drm_node_gpu = "/dev/dri/card0"
+        # Parse arguments.
+        for key in kwargs.keys():
+            setattr(self, key, kwargs[key])
+
+    def compose(self):
+        def as_arg(value):
+            return '--%s' % value.replace('_', '-')
+
+        result = [self.program]
+        for attr in vars(self):
+            value = getattr(self, attr)
+            if isinstance(value, bool):
+                if value == True:
+                    result.append(as_arg(attr))
+            else:
+                result.append(as_arg(attr))
+                result.append(str(value))
+        return result
+
+    def run(self):
+        try:
+            subprocess.call([self.program])
+        except FileNotFoundError:
+            sys.stderr.write("Fatal: %s is not installed\n" % self.program)
+            return 
+
+        try:
+            output = subprocess.run(" ".join(self.compose()), shell=True, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+        except Exception as e:
+            return
+
+        for line in output.stdout.splitlines():
+            m = re.match(r"Rendered .* \(([0-9.]+) fps\)$", line.decode('utf-8'))
+            if m:
+                fps = float(m.group(1))
+        return fps
+
+def run_wpetestbed_unbounded():
+    return WPETestbed(unbounded=True).run()
+
+def run_wpetestbed_unbounded_and_no_animate():
+    return WPETestbed(unbounded=True, no_animate=True).run()
+
+TESTS_DICT = {
+    'unbounded': run_wpetestbed_unbounded,
+    'unbounded_and_no_animate': run_wpetestbed_unbounded_and_no_animate,
+}
+
+def run_tests(key=None):
+    result = {}
+    if key:
+        if key in TESTS_DICT.keys():
+            callback = TESTS_DICT[key]
+            value = callback()
+            if value:
+                result[key] = value
+    else:
+        for key in TESTS_DICT.keys():
+            callback = TESTS_DICT[key]
+            value = callback()
+            if value:
+                result[key] = value
+    return result
+
+def read_baseline():
+    filename = get_baseline_filename()
+    with open(filename, "r+") as fd:
+        return json.load(fd)
+
+def update_baseline(results):
+    filename = get_baseline_filename()
+    with open(filename, "w+") as fd:
+        fd.write(json.dumps(results))
+
+def get_baseline_path(dirname):
+    root = os.path.dirname(os.path.abspath(__file__))
+    dirname = os.path.join(root, "..", "tests-baselines", dirname)
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+    filename = "%s-expected.json" % get_platform()
+    return os.path.realpath(os.path.join(dirname, filename))
+
+def get_baseline_filename():
+    return get_baseline_path("wpe-testbed")
+
+def get_platform():
+    hostname = os.uname().nodename
+
+    if "pi3" in hostname:
+        return "rpi3"
+    elif "pi5" in hostname:
+        return "rpi5"
+    elif "wandboard" in hostname:
+        return "wandboard"
+    else:
+        return "other"
+
+def dump(results):
+	print(json.dumps(results))
+
+def main(argument_list):
+    args = parse_args(argument_list)
+
+    actual_results = run_tests(args.filter)
+    if args.new_baseline:
+        update_baseline(actual_results)
+    elif args.report:
+        expected_results = read_baseline()
+        if args.filter:
+            dump({'actual': actual_results[args.filter], 'expected': expected_results[args.filter]})
+        else:
+            dump({'actual': actual_results, 'expected': expected_results})
+    else:
+        if actual_results:
+            dump(actual_results)
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
